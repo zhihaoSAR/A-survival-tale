bfd100db36e3111832343c7d7d2e57141dd79374
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2020-02-16 20:29:00 +0100

    crear repositorio

---
+++ Assets/Scenes/LoadTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTest : MonoBehaviour
+{
+    static bool once = false;
+    Scene[] myscenes;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.E) & !once)
+        {
+            once = true;
+            //SceneManager.LoadSceneAsync("Scene2");
+            SceneManager.LoadScene("Scene2", LoadSceneMode.Additive);
+            myscenes = SceneManager.GetAllScenes();
+        }
+        if(Input.GetKey(KeyCode.B))
+        {
+            SceneManager.UnloadSceneAsync(myscenes[0]);
+        }
+    }
+}
