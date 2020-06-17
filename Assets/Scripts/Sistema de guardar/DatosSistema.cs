46718876b13b7ad0f9f320c481bd6b5bbcc99a20
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2020-06-17 22:32:08 +0200

    comprobar la funcionalidad

--- Assets/Scripts/Sistema de guardar/DatosSistema.cs
+++ Assets/Scripts/Sistema de guardar/DatosSistema.cs
@@ -64,8 +64,8 @@ public class DatosSistema
         //-----------------principal---------------------------
         invencibilidad = 0;
         //---------------control---------------------
-        tipoControl = 0;
-        inputTime = 1f;
+        tipoControl = 2;
+        inputTime = 0.5f;
         velocidad = 1;
         //------------------contraste--------------
         color_fondo = Color.clear;
