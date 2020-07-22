ea6fae41ecbde892aab740c773f507ed8fa295cd
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2020-07-22 15:08:34 +0200

    activa desactiva animacion hecho

---
+++ Assets/Scripts/AnimacionEfecto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionEfecto : MonoBehaviour
+{
+    public Material agua;
+    Vector4 aguaVel = new Vector4(-3, 2, 1, 3);
+    public void ActualizaAnimacion(int activa)
+    {
+        if(activa == 0)
+        {
+            agua.SetVector("_GSpeed", aguaVel);
+        }
+        else
+        {
+            agua.SetVector("_GSpeed", Vector4.zero);
+        }
+    }
+}
